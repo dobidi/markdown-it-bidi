764cf6f8c0668acc84be2840f129919bcd54cde1
${Mr.DJA} <42304709+iMrDJAi@users.noreply.github.com>
2022-03-24 14:57:16 +0100

    Better implementation for lists

--- index.js
+++ index.js
@@ -5,23 +5,70 @@ module.exports = function markdownItBidi(md) {
     'paragraph_open',
     'bullet_list_open',
     'ordered_list_open',
+    'bullet_list_close',
+    'ordered_list_close',
     'table_open',
     'th_open',
     'td_open'
   ];
 
+  const isInactiveRangeStart = token => {
+    return [
+      'bullet_list_open',
+      'ordered_list_open'
+    ].includes(token.type);
+  };
+
+  const isInInactiveRange = (token, env) => {
+    const inactiveRangeStart = env.inactiveRangeStart;
+
+    if (!inactiveRangeStart) {
+      return false;
+    }
+
+    if (
+      token.nesting === -1
+      && token.tag === inactiveRangeStart.tag
+      && token.level === inactiveRangeStart.level
+    ) {
+      delete env.inactiveRangeStart;
+      return false;
+    }
+
+    return true;
+  };
+
+  const isFirstChildInBlockquote = prevToken =>
+    (prevToken && prevToken.type === 'blockquote_open');
+
+  const isFirstThInTable = (token, prevToken) => 
+    (token.type === 'th_open' && prevToken.type === 'tr_open');
+
   const bidi = defaultRenderer => (tokens, idx, opts, env, self) => {
     const token = tokens[idx];
     const prevToken = tokens[idx - 1];
-    if (token.type === 'th_open' && prevToken.type === 'tr_open') {
-      return defaultRenderer(tokens, idx, opts, env, self);
+
+    if (
+      !isInInactiveRange(token, env)
+      && token.nesting === 1
+      && !isFirstChildInBlockquote(prevToken)
+      && !isFirstThInTable(token, prevToken)
+    ) {
+      token.attrSet('dir', 'auto');
     }
-    token.attrSet('dir', 'auto');
+
+    if (
+      !env.inactiveRangeStart
+      && isInactiveRangeStart(token)
+    ) {
+      env.inactiveRangeStart = token;
+    }
+
     return defaultRenderer(tokens, idx, opts, env, self);
   };
 
-  const proxy = (tokens, idx, opts, _, self) => {
-    return self.renderToken(tokens, idx, opts);
+  const proxy = (tokens, idx, opts, env, self) => {
+    return self.renderToken(tokens, idx, opts, env, self);
   };
 
   rules.forEach(rule => {
